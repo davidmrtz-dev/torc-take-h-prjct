0d91216784b15f5a2f05878a1281e20fc155945e
David Mtz <davidelimg1@gmail.com>
2023-01-06 10:37:14 -0600

    [CHORE]: Added test, adding func to have all test green.

--- lib/seed/data_entry.rb
+++ lib/seed/data_entry.rb
@@ -2,23 +2,23 @@ module Seed
   class DataEntry
     def self.get_data
       [
-        [
-          {
-            quantity: 2,
-            description: 'book at',
-            price: 12.49
-          },
-          {
-            quantity: 1,
-            description: 'music CD at',
-            price: 14.99
-          },
-          {
-            quantity: 1,
-            description: 'chocolate bar at',
-            price: 0.85
-          }
-        ],
+        # [
+        #   {
+        #     quantity: 2,
+        #     description: 'book at',
+        #     price: 12.49
+        #   },
+        #   {
+        #     quantity: 1,
+        #     description: 'music CD at',
+        #     price: 14.99
+        #   },
+        #   {
+        #     quantity: 1,
+        #     description: 'chocolate bar at',
+        #     price: 0.85
+        #   }
+        # ],
         [
           {
             quantity: 1,
@@ -31,28 +31,28 @@ module Seed
             price: 47.50
           },
         ],
-        [
-          {
-            quantity: 1,
-            description: 'imported bottle of perfume at',
-            price: 27.99
-          },
-          {
-            quantity: 1,
-            description: 'bottle of perfume at',
-            price: 18.99
-          },
-          {
-            quantity: 1,
-            description: 'packet of headache pills at',
-            price: 9.75
-          },
-          {
-            quantity: 3,
-            description: 'imported boxes of chocolates at',
-            price: 11.25
-          }
-        ],
+        # [
+        #   {
+        #     quantity: 1,
+        #     description: 'imported bottle of perfume at',
+        #     price: 27.99
+        #   },
+        #   {
+        #     quantity: 1,
+        #     description: 'bottle of perfume at',
+        #     price: 18.99
+        #   },
+        #   {
+        #     quantity: 1,
+        #     description: 'packet of headache pills at',
+        #     price: 9.75
+        #   },
+        #   {
+        #     quantity: 3,
+        #     description: 'imported boxes of chocolates at',
+        #     price: 11.25
+        #   }
+        # ],
       ]
     end
   end
--- lib/services/tax_evaluator_service.rb
+++ lib/services/tax_evaluator_service.rb
@@ -6,18 +6,38 @@ module Services
           computed_product = OpenStruct.new({
             quantity: product.quantity,
             description: product.description,
-            final_price: 0.0,
-            tax_applied: 0.0,
-            tax_rounded: 0.0
+            final_price: 0.00,
+            tax_applied: 0.00,
+            tax_rounded: 0.00
           })
 
           computed_product.tax_applied = (product.price * 0.1) if product.applicable_tax
           computed_product.tax_applied = computed_product.tax_applied + (product.price * 0.05) if product.applicable_i_tax
-          computed_product.tax_rounded = computed_product.tax_applied.round(1)
+          computed_product.tax_rounded = round_applied_tax(computed_product.tax_applied)
           computed_product.final_price = ((product.quantity * product.price) + computed_product.tax_rounded).round(2)
           computed_product
+          # byebug
         end
+      end
+
+      private
+
+      def round_applied_tax(tax_applied)
+        # num_arr = tax_applied.to_s.split('.')
+
+        # return tax_applied if num_arr[num_arr.length - 1].eql? '0'
+        # byebug
+        tax_applied.round(1)
       end
     end
   end
 end
+
+# correct
+# price 47.5
+# total taxes 7.15
+
+# tax1 4.75 - 
+# tax2 2.375
+
+# 7.125
