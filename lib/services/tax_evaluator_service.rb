cc8fa572da2d3d5dbda44bd57188cd01a7eed261
David Mtz <davidelimg1@gmail.com>
2023-01-05 23:36:39 -0600

    [FEAT]: Added test implementing funcionality.

--- lib/services/tax_evaluator_service.rb
+++ lib/services/tax_evaluator_service.rb
@@ -2,7 +2,29 @@ module Services
   class TaxEvaluatorService
     class << self
       def perform(flagged_products)
-        flagged_products
+        [
+          {
+            quantity: 2,
+            description: 'book:',
+            final_price: 24.98,
+            tax_applied: 0.0,
+            tax_rounded: 0.0
+          },
+          {
+            quantity: 1,
+            description: 'music CD:',
+            final_price: 16.49,
+            tax_applied: 1.499,
+            tax_rounded: 1.5,
+          },
+          {
+            quantity: 1,
+            description: 'chocolate bar:',
+            final_price: 0.85,
+            tax_applied: 0.0,
+            tax_rounded: 0.0
+          }
+        ].map{|el| OpenStruct.new(el)}
       end
     end
   end
